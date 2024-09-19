b72547872fe026fb87e55b8a20090adb864212ba
kentrist <kennersantos9@gmail.com>
2024-09-18 22:02:25 -0400

    ataualizações

---
+++ execicos/ex25/test.py
@@ -0,0 +1,13 @@
+#testando o hello word
+print("hello world")
+#vamos testar o input 
+
+x = 10
+nome = 'aluno'
+nota = 8.75
+fez_incricao= True
+
+print(type(x))
+print(type(nome))
+print(type(nota))
+print(type(fez_incricao))
